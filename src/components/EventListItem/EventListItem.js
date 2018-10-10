adb0e6649d32e55ec9753ae1fadf5bb3477894c9
rmrotek <remek87@gmail.com>
2018-10-10 16:34:26 +0200

    making state, getting img from public

--- src/components/EventListItem/EventListItem.js
+++ src/components/EventListItem/EventListItem.js
@@ -3,13 +3,20 @@ import './EventListItem.css';
 
 class EventListItem extends Component {
   
-  
+  state = {
+    id: 1,
+    img: '/event-icons/PH.png',
+    title: 'event one',
+
+
+  }
   
   
   render () {
     return (
       <>
       <div>event item</div>
+      <img src={this.state.img} alt='event icon'/>
       </>
     )
   }
