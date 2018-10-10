b78f51aaff0fe278ad36481a90cf699239ee1053
rmrotek <remek87@gmail.com>
2018-10-10 16:15:38 +0200

    event list item exported and imported in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import EventListItem from '../EventListItem/EventListItem'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>Hello</div>
+      <>
+        <div>Hello</div>
+        <EventListItem />
+      </>
+
     );
   }
 }
---
+++ src/components/EventListItem/EventListItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './EventListItem.css';
+
+class EventListItem extends Component {
+  
+  
+  
+  
+  render () {
+    return (
+      <>
+      <div>event item</div>
+      </>
+    )
+  }
+}
+
+export default EventListItem
