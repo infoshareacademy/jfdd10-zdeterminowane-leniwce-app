38ddba7f288282d004b319adf3846e23e3a9576e
Pawel <paweljozwiak1986@gmail.com>
2018-10-10 16:45:26 +0200

    Inital commit

---
+++ src/components/EventParticipantListItem/EventParticipantListItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class EventParticipantListItem extends Component {
+  render() {
+    return (
+      <div className="EventParticipantListItem">
+      </div>
+    )
+  }
+}
+
+export default EventParticipantListItem
