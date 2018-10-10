3c6dd201127dc55239714949b87c87ad870d793b
rmrotek <remek87@gmail.com>
2018-10-10 16:39:43 +0200

    PH state done

--- src/components/EventListItem/EventListItem.js
+++ src/components/EventListItem/EventListItem.js
@@ -2,21 +2,25 @@ import React, { Component } from 'react';
 import './EventListItem.css';
 
 class EventListItem extends Component {
-  
+
   state = {
     id: 1,
     img: '/event-icons/PH.png',
     title: 'event one',
+    description: 'short description for event number one',
 
 
   }
-  
-  
-  render () {
+
+
+  render() {
     return (
       <>
-      <div>event item</div>
-      <img src={this.state.img} alt='event icon'/>
+        <div>event item</div>
+        <div>{this.state.title}</div>
+        <img src={this.state.img} alt='event icon' />
+        <div>{this.state.description}</div>
+
       </>
     )
   }
