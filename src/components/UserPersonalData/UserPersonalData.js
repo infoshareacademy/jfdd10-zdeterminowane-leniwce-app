f67e3454409467c6f6fe33b93ed04e726493d162
KMDzik <katarzyna.dzik90@gmail.com>
2018-10-10 16:57:24 +0200

    wip

---
+++ src/components/UserPersonalData/UserPersonalData.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class UserPersonalData extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+
+export default UserPersonalData;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import UserPersonalData from './UserPersonalData/UserPersonalData';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
