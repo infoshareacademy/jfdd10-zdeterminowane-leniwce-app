342ab6d74a08cea8c3a5777ae6ea975563fe5461
rmrotek <remek87@gmail.com>
2018-10-11 16:16:22 +0200

    event searcher boilerplate

---
+++ src/components/EventSearcher/EventSearcher.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import EventSearcherResults from '../EventSearcherResults/EventSearcherResults'
+
+class EventSearcher extends Component {
+
+ 
+
+  render() {
+    return (
+      <>
+        <EventSearcherResults />
+
+      </>
+    )
+  }
+}
+
+export default EventSearcher
---
+++ src/components/EventSearcherResults/EventSearcherResults.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import EventList from '../EventList/EventList'
+
+
+class EventSearcherResults extends Component {
+
+ 
+
+  render() {
+    return (
+      <>
+        <EventList />
+
+      </>
+    )
+  }
+}
+
+export default EventSearcherResults
