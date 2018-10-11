999a7ad8aea331259d1d26d13af1772f6785513c
rmrotek <remek87@gmail.com>
2018-10-11 14:35:15 +0200

    md file added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 import EventList from '../EventList/EventList';
+import './App.css';
 
 class App extends Component {
 
-  
+
   render() {
     return (
       <>
