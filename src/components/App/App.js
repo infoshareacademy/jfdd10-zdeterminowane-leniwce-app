14af6e6eb46506114e0bbce6930b16cc52fc9a24
KMDzik <katarzyna.dzik90@gmail.com>
2018-10-11 16:36:42 +0200

    personal data set up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserPersonalData from '../UserPersonalData/UserPersonalData'
 
 class App extends Component {
   render() {
     return (
-      <div>Hello</div>
+      <UserPersonalData foo={'bar'}/>
     );
   }
 }
--- src/components/UserPersonalData/UserPersonalData.js
+++ src/components/UserPersonalData/UserPersonalData.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class UserPersonalData extends Component {
   render() {
     return (
-      <div>Hello</div>
+      <div>Dzik</div>
     );
   }
 }
