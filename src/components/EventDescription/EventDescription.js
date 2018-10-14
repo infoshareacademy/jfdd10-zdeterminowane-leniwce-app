5c78569d631647972c39f82dd6e0606fa0af5bd9
Pawel <paweljozwiak1986@gmail.com>
2018-10-14 20:47:04 +0200

    join button added

--- src/components/EventDescription/EventDescription.js
+++ src/components/EventDescription/EventDescription.js
@@ -1,10 +1,26 @@
-import React, { Component } from 'react'
-import './EventDescription.css'
+import React, { Component } from "react";
+import "./EventDescription.css";
 
 class EventDescription extends Component {
   render() {
     return (
-      <div>kasza</div>
+      <div className="event__description">
+        <img
+          className="event__image"
+          src="http://placehold.jp/150x150.png"
+          alt=""
+        />
+        <div className="event__description--text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          fringilla elementum justo, scelerisque feugiat ligula tristique
+          convallis. Maecenas lectus nulla, venenatis vel sagittis vitae,
+          sodales eget dui. Proin a ex sed diam cursus efficitur sit amet
+          lacinia tortor. Maecenas tincidunt euismod vestibulum. Suspendisse vel
+          convallis quam, sed rhoncus lacus. Aliquam ornare arcu sapien, semper
+          ultrices ipsum ultricies at. Maecenas tincidunt efficitur interdum.
+        </div>
+      </div>
+      
     );
   }
 }
---
+++ src/components/EventDescriptionJoin/EventDescriptionJoin.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./EventDescriptionJoin.css";
+
+class EventDescriptionJoin extends Component {
+  render() {
+    return (
+
+    <div className="event__description--buttonarea">
+      <button className="event__description--button">Join</button>
+    </div>
+  
+      
+    );
+  }
+}
+
+export default EventDescriptionJoin;
