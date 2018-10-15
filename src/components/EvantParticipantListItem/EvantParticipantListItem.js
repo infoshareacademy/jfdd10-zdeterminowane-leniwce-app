4b84ce22d5757fe21b9b5fb42adead9bf3e6ae1e
Pawel <paweljozwiak1986@gmail.com>
2018-10-15 16:55:47 +0200

    EvantParticipantListItem

---
+++ src/components/EvantParticipantListItem/EvantParticipantListItem.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./EvantParticipantListItem.css";
+
+class EvantParticipantListItem extends Component {
+  render() {
+    return (
+      <div>aushiash</div>
+    );
+  }
+}
+
+export default EvantParticipantListItem;
