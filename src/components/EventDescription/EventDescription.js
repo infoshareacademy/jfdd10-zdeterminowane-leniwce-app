f6090deb82d2f59a9c69df1c2075d787257a09ca
Pawel <paweljozwiak1986@gmail.com>
2018-10-15 16:26:05 +0200

    EventDescription Done

--- src/components/EventDescription/EventDescription.js
+++ src/components/EventDescription/EventDescription.js
@@ -4,23 +4,18 @@ import "./EventDescription.css";
 class EventDescription extends Component {
   render() {
     return (
-      <div className="event__description">
-        <img
-          className="event__image"
-          src="http://placehold.jp/150x150.png"
-          alt=""
-        />
-        <div className="event__description--text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          fringilla elementum justo, scelerisque feugiat ligula tristique
-          convallis. Maecenas lectus nulla, venenatis vel sagittis vitae,
-          sodales eget dui. Proin a ex sed diam cursus efficitur sit amet
-          lacinia tortor. Maecenas tincidunt euismod vestibulum. Suspendisse vel
-          convallis quam, sed rhoncus lacus. Aliquam ornare arcu sapien, semper
-          ultrices ipsum ultricies at. Maecenas tincidunt efficitur interdum.
+      <div>
+        <div className="EventDescription-description">
+        <img className="EventDescription-image" src={this.props.photoUrl} alt=""
+        /> 
+          <div className="EventDescription-description--text">
+            {this.props.content}
+          </div>
+        </div>
+        <div className="EventDescription-description--buttonarea">
+          <button className="EventDescription-description--button">Join</button>
         </div>
       </div>
-      
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import EventParticipantListItem from './components/EventParticipantListItem/EventParticipantListItem'
-import EventView  from './components/EventView/EventView'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
