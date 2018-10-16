3dfd03c02e45333265535f8d347403e8c447ea46
rmrotek <remek87@gmail.com>
2018-10-16 15:32:38 +0200

    styling for main view

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,52 @@
 
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+
 import HomeView from '../HomeView/HomeView';
 import './App.css';
 
-
 class App extends Component {
 
-  
+
 
   render() {
 
     return (
-      <div className='app-container'>
-        <h1>Lazyness Radar</h1>
-        <HomeView  />
-        <h1>KOmponent 1</h1>
-        <h1>KOmponent 2</h1>
-        <h1>KOmponent 3</h1>
-      </div>
+      <>
+
+
+        <Router>
+          <div className='app-container'>
+            <h1>Lazyness Radar</h1>
+            <div className='app-wrapper'>
+
+              <div className='app-links'>
+                <p>
+                  <NavLink exact to='/'>
+                    Strona główna
+              </NavLink>
+                </p>
+
+              </div>
+
+              <div className='main-app-window'>
+                <Route exact path="/" component={() => <h1>Hello</h1>} />
+
+
+                <HomeView />
+                <h1>KOmponent 1</h1>
+                <h1>KOmponent 2</h1>
+                <h1>KOmponent 3</h1>
+              </div>
+            </div>
+
+
+
+          </div>
+
+        </Router>
+      </>
+
 
     );
 
