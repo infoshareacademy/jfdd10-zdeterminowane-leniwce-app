03d25127171ac2db9314d6f40344223c92077a6b
KMDzik <katarzyna.dzik90@gmail.com>
2018-10-16 15:52:18 +0200

    UserEvents component wip

---
+++ src/components/UserEvents/UserEvents.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./UserEvents.css";
+import EventList from './EventList'
+
+class UserEvents extends Component {
+
+    // todo: trzeba zaimplementować dane!
+    
+
+  render() {
+    return (
+      <EventList></EventList>
+      
+    );
+  }
+}
+
+export default UserEvents;
