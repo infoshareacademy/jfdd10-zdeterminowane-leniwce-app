874ab3892cdb2cf1396aab3176b93224a37f69c0
rmrotek <remek87@gmail.com>
2018-10-19 14:58:53 +0200

    code clean in eventsearcher

--- src/components/EventSearcher/EventSearcher.js
+++ src/components/EventSearcher/EventSearcher.js
@@ -14,8 +14,6 @@ class EventSearcher extends Component {
       query: query
     })
 
-
-
   // getFilteredEvents2 = () => this.state.events.filter((event) => {
   //     let eventTitle = event.title.toLowerCase();
   //     let eventDescription = event.description.toLowerCase();
@@ -27,34 +25,25 @@ class EventSearcher extends Component {
       phrase => phrase.toLowerCase()
     ).some(phrase => phrase.includes(this.state.query.toLocaleLowerCase())))
 
-
-
   render() {
     return (
       <>
         <EventSearcherForm getSearchQuery={this.setSearchQuery} />
 
-        {
+        {/* {
           this.state.query &&
           <p>
             Searched for: &nbsp;<span>{this.state.query}</span>
           </p>
-        }
-
+        } */}
 
         <Grid container justify='center'>
-
           <Grid item md={10} lg={8} sm>
             <Paper>
-
               <EventSearcherResults eventsData={this.getFilteredEvents()} searchQuery={this.state.query} />
             </Paper>
-
-
           </Grid>
         </Grid>
-
-
       </>
     )
   }
