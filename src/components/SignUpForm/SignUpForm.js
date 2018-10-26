2ef50630665e40cb983364e92c5023b2885fee3e
rmrotek <remek87@gmail.com>
2018-10-26 16:44:30 +0200

    soke magic by Bartosz

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import firebase from 'firebase'
 
+const makeNewUser = (userId, { name, surname, email }) => {
+  firebase.database().ref('users/' + userId).set({
+    first_name: name,
+    last_name: surname,
+    email: email,
+    avatar: '',
+    description: ''
+  })
+}
 
 
 class SignUpForm extends Component {
@@ -25,60 +34,51 @@ class SignUpForm extends Component {
       this.state.email,
       this.state.password
     ).then(
-      ({user}) => {
+      ({ user }) => {
         this.setState({ error: null });
-        this.makeNewUser(user.uid, this.state.name, this.state.surname, this.state.email)
+        makeNewUser(user.uid, this.state)
       }
     ).catch(
       error => this.setState({ error })
     )
   }
 
-  makeNewUser = (userId, name, surname, email) => {
-  firebase.database().ref('users/' + userId).set({
-    first_name: name,
-    last_name: surname,
-    email: email,
-    avatar: '',
-    description: ''
-  })
-}
+  
 
 
 
-
-render() {
-  return (
-    <form onSubmit={this.handleSubmit} className="SignUpForm">
-      {this.state.error && <p>{this.state.error.message}</p>}
-      <input
-        placeholder="Enter name"
-        name="name"
-        value={this.state.name}
-        onChange={this.handleChange}
-      />
-      <input
-        placeholder="Enter surname"
-        name="surname"
-        value={this.state.surname}
-        onChange={this.handleChange}
-      />
-      <input
-        placeholder="Enter email"
-        name="email"
-        value={this.state.email}
-        onChange={this.handleChange}
-      />
-      <input
-        placeholder="Enter password"
-        name="password"
-        value={this.state.password}
-        onChange={this.handleChange}
-      />
-      <button>Sign up</button>
-    </form>
-  );
-}
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="SignUpForm">
+        {this.state.error && <p>{this.state.error.message}</p>}
+        <input
+          placeholder="Enter name"
+          name="name"
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <input
+          placeholder="Enter surname"
+          name="surname"
+          value={this.state.surname}
+          onChange={this.handleChange}
+        />
+        <input
+          placeholder="Enter email"
+          name="email"
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+        <input
+          placeholder="Enter password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+        />
+        <button>Sign up</button>
+      </form>
+    );
+  }
 }
 
 export default SignUpForm;
