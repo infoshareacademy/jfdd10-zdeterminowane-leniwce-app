a0de75cfb6c44df0c9d8fc8f2d96ec733f3000f4
KMDzik <katarzyna.dzik90@gmail.com>
2018-10-29 16:37:00 +0100

    Validation of Sign In Form done

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import firebase from 'firebase'
 
 class SignInForm extends Component {
   state = {
-    name: "",
-    surname: "",
     email: "",
     password: "",
     error: null
@@ -17,19 +16,35 @@ class SignInForm extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    console.log(event);
-    // firebase.auth().createUserWithEmailAndPassword(
-    //   this.state.email,
-    //   this.state.password
-    // ).then(
-    //   () =>  this.setState({ error: null })
-    // ).catch(
-    //   error => this.setState({ error })
-    // )
+    console.log(this.state);
+
+    if (this.state.email.length === 0) {
+      return this.setState ({
+        error: 'Please add your email'
+      })
+    }
+
+    if (this.state.password.length === 0) {
+      return this.setState({
+        error: 'Please add your password'
+      })
+    }
+    
+    firebase.auth().createUserWithEmailAndPassword(
+      this.state.email,
+      this.state.password
+    ).then (
+      () => this.setState({ error: null})
+    ).catch(
+      error => {
+        this.setState({error: error.message})
+      }
+    )
   }
 
   render() {
     return (
+      <Fragment>
       <form onSubmit={this.handleSubmit} className="SignUpForm">
         {this.state.error && <p>{this.state.error.message}</p>}
         <input
@@ -41,11 +56,16 @@ class SignInForm extends Component {
         <input
           placeholder="Enter password"
           name="password"
+          type="password"
           value={this.state.password}
           onChange={this.handleChange}
         />
         <button>Sign in</button>
       </form>
+      {
+        this.state.error && <div style={{color: 'red'}}>{this.state.error}</div>
+      }
+        </Fragment>
     );
   }
 }
