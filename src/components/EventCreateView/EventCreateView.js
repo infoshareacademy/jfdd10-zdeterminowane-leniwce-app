c44748eed56d13887503dc167d9155a2a8c7d7ac
rmrotek <remek87@gmail.com>
2018-10-31 16:23:28 +0100

    added paper to creave event

--- src/components/EventCreateView/EventCreateView.js
+++ src/components/EventCreateView/EventCreateView.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { withAuthContext } from "../../contexts/AuthContext";
-import { Grid, TextField, Button } from "@material-ui/core";
+import { Grid, TextField, Button, Paper } from "@material-ui/core";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import firebase from 'firebase'
 import BackButton from "../BackButton/BackButton";
@@ -89,99 +89,102 @@ class EventCreateView extends Component {
 
     return (
       <>
-        <Grid container justify='center' >
-          <form onSubmit={this.handleSubmit}>
-            <Grid container item justify='center' spacing={8} >
-              <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                <Map
-                  onClick={this.addMarker}
-                  center={position}
-                  zoom={13}
-                  style={{ height: 350 }}>
-                  <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        <Grid container justify='center' spacing={8} >
+
+          <Paper>
+            <form onSubmit={this.handleSubmit}>
+              <Grid container item justify='center' spacing={8} >
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <Map
+                    onClick={this.addMarker}
+                    center={position}
+                    zoom={13}
+                    style={{ height: 350 }}>
+                    <TileLayer
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    />
+                    <Marker position={position}>
+                      <Popup>
+                        {this.state.title}
+                      </Popup>
+                    </Marker>
+                  </Map>
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <TextField
+                    fullWidth={true}
+                    color='secondary'
+                    variant='outlined'
+                    label="Title"
+                    name="title"
+                    value={this.state.title}
+                    onChange={this.handleChange}
                   />
-                  <Marker position={position}>
-                    <Popup>
-                      {this.state.title}
-                    </Popup>
-                  </Marker>
-                </Map>
-              </Grid>
+                </Grid>
 
-              <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                <TextField
-                  fullWidth={true}
-                  color='secondary'
-                  variant='outlined'
-                  label="Title"
-                  name="title"
-                  value={this.state.title}
-                  onChange={this.handleChange}
-                />
-              </Grid>
+                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
 
-              <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <TextField
+                    fullWidth={true}
+                    color='secondary'
+                    variant='outlined'
+                    label="Description"
+                    name="description"
+                    multiline
+                    rows='3'
+                    value={this.state.description}
+                    onChange={this.handleChange}
+                  />
+                </Grid>
 
-                <TextField
-                  fullWidth={true}
-                  color='secondary'
-                  variant='outlined'
-                  label="Description"
-                  name="description"
-                  multiline
-                  rows='3'
-                  value={this.state.description}
-                  onChange={this.handleChange}
-                />
-              </Grid>
+                <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
 
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+                  <TextField
+                    fullWidth={true}
+                    color='secondary'
+                    variant='outlined'
+                    label="Location X"
+                    name="locationX"
+                    type='number'
+                    value={this.state.locationX}
+                    onChange={this.handleChange}
+                  />
+                </Grid>
 
-                <TextField
-                  fullWidth={true}
-                  color='secondary'
-                  variant='outlined'
-                  label="Location X"
-                  name="locationX"
-                  type='number'
-                  value={this.state.locationX}
-                  onChange={this.handleChange}
-                />
-              </Grid>
+                <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
 
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-
-                <TextField
-                  fullWidth={true}
-                  color='secondary'
-                  variant='outlined'
-                  label="Location Y"
-                  name="locationY"
-                  type='number'
-                  value={this.state.locationY}
-                  onChange={this.handleChange}
-                />
-              </Grid>
-              <Grid item xs={12} sm={3} md={2} lg={2} xl={2}>
-               <BackButton />
-              </Grid>
-              <Grid item xs={12} sm={9} md={10} lg={10} xl={10}>
-                <Button
-                  type='submit'
-                  variant='contained'
-                  size='large'
-                  color='primary'
-                  fullWidth={true}
-                >
-                  Create Event
+                  <TextField
+                    fullWidth={true}
+                    color='secondary'
+                    variant='outlined'
+                    label="Location Y"
+                    name="locationY"
+                    type='number'
+                    value={this.state.locationY}
+                    onChange={this.handleChange}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={3} md={2} lg={2} xl={2}>
+                  <BackButton />
+                </Grid>
+                <Grid item xs={12} sm={9} md={10} lg={10} xl={10}>
+                  <Button
+                    type='submit'
+                    variant='contained'
+                    size='large'
+                    color='primary'
+                    fullWidth={true}
+                  >
+                    Create Event
                 </Button>
+                </Grid>
               </Grid>
 
-            </Grid>
+            </form>
+          </Paper>
 
-          </form>
 
         </Grid>
         <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
